e184332e448ce9b941bbfeabeafce8d6c39cd3ba
Jose Rubio <jadrubio@gmail.com>
2024-06-02 16:53:17 -0400

    Refactored FormInput to allow for textarea styl input for notes and implemented

--- src/UI/FormInput.tsx
+++ src/UI/FormInput.tsx
@@ -7,25 +7,53 @@ interface FormInputProps {
   label: string;
   control: Control<any>;
   errors: FieldErrors;
+  type?: "textfield" | "textarea";
   xs?: GridSize;
   sm?: GridSize;
+  minRows?: number;
+  maxRows?: number;
 }
 
-const FormInput = ({ name, label, control, errors, xs = 12, sm }: FormInputProps) => (
+const FormInput = ({
+                     name,
+                     label,
+                     control,
+                     errors,
+                     type = "textfield",
+                     xs = 12,
+                     sm,
+                     minRows,
+                     maxRows,
+                   }: FormInputProps) => (
   <Grid item xs={xs} sm={sm}>
     <Controller
       name={name}
       control={control}
       render={({ field }) => (
-        <TextField
-          {...field}
-          label={label}
-          variant="outlined"
-          error={!!errors[name]}
-          helperText={errors[name]?.message as string}
-          fullWidth
-          sx={{ backgroundColor: "white" }}
-        />
+        type === "textarea" ? (
+          <TextField
+            {...field}
+            label={label}
+            variant="outlined"
+            error={!!errors[name]}
+            helperText={errors[name]?.message as string}
+            fullWidth
+            multiline
+            minRows={minRows}
+            maxRows={maxRows}
+            sx={{ backgroundColor: "white" }}
+          />
+        ) : (
+          <TextField
+            {...field}
+            label={label}
+            variant="outlined"
+            error={!!errors[name]}
+            helperText={errors[name]?.message as string}
+            fullWidth
+            sx={{ backgroundColor: "white" }}
+          />
+        )
       )}
     />
   </Grid>
--- src/features/profile/ProfileForm.tsx
+++ src/features/profile/ProfileForm.tsx
@@ -1,37 +1,50 @@
-import store from '../../store';
+import store from "../../store";
 import { useSelector } from "react-redux";
-import { useForm, Controller } from "react-hook-form";
+import {Controller, useForm} from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { Box, Button, TextField, Typography, Grid } from "@mui/material";
-import {blankFormState, Profile, validationSchema} from "./profileUtils";
+import {Box, Button, Typography, Grid, TextareaAutosize} from "@mui/material";
+import {blankFormState, Profile, transformObjValNullToEmptyString, validationSchema} from "./profileUtils";
 import { RootState } from "../../store";
-import {createProfile, setActiveProfile, updateProfile} from "./profileSlice";
-import {FormInput} from "../../UI/FormInput";
+import { createProfile, setActiveProfile, updateProfile } from "../../store/profileSlice";
+import { FormInput } from "../../UI/FormInput";
+import {useEffect} from "react";
 
-type ProfileForm = Omit<Profile, "id">;
+type ProfileFormArgs = Omit<Profile, "id">;
 
 const ProfileForm = () => {
   const profile = useSelector((state: RootState) => state.profile.inFocus);
   const loading = useSelector((state: RootState) => state.profile.loading);
+  const navigate = useNavigate();
 
   const {
     control,
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<ProfileForm>({
+  } = useForm<ProfileFormArgs>({
     resolver: yupResolver(validationSchema),
-    defaultValues: blankFormState,
+    defaultValues: profile ? transformObjValNullToEmptyString(profile) : blankFormState,
   });
+
+  useEffect(() => {
+    if (profile) {
+      reset(profile);
+    } else {
+      reset(blankFormState);
+    }
+  }, [profile, reset]);
 
   const handleClear = () => {
     reset(blankFormState);
     store.dispatch(setActiveProfile(null));
   };
 
-  const onSubmit = (data: ProfileForm) => {
+  const onSubmit = (data: ProfileFormArgs) => {
     if (profile && profile?.id) {
-      store.dispatch(updateProfile({ id: profile.id, data: { id: profile.id, ...data } }));
+      store.dispatch(
+        updateProfile({ id: profile.id, data: { id: profile.id, ...data } }),
+      );
     } else {
       store.dispatch(createProfile(data));
     }
@@ -44,23 +57,96 @@ const ProfileForm = () => {
       onSubmit={handleSubmit(onSubmit)}
       sx={{ mt: 3, width: "100%", maxWidth: "600px", mx: "auto" }}
     >
-      <Typography variant="h4" component="h1" gutterBottom>
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
+        style={{ textAlign: "center" }}
+      >
         {profile?.id ? "Update Profile" : "Create Profile"}
       </Typography>
       {loading && <Typography variant="h6">Loading...</Typography>}
       {!loading && (
         <Grid container spacing={2}>
-          <FormInput name="first_name" label="First Name" control={control} errors={errors} sm={6} />
-          <FormInput name="last_name" label="Last Name" control={control} errors={errors} sm={6} />
-          <FormInput name="photo" label="Photo URL" control={control} errors={errors} />
-          <FormInput name="address" label="Address" control={control} errors={errors} />
-          <FormInput name="city" label="City" control={control} errors={errors} sm={6} />
-          <FormInput name="state" label="State" control={control} errors={errors} sm={3} />
-          <FormInput name="zip" label="ZIP" control={control} errors={errors} sm={3} />
-          <FormInput name="phone" label="Phone" control={control} errors={errors} />
-          <FormInput name="email" label="Email" control={control} errors={errors} />
-          <FormInput name="notes" label="Notes" control={control} errors={errors} />
-          <Grid item xs={12} sm={6}>
+          <FormInput
+            name="first_name"
+            label="First Name"
+            control={control}
+            errors={errors}
+            sm={6}
+          />
+          <FormInput
+            name="last_name"
+            label="Last Name"
+            control={control}
+            errors={errors}
+            sm={6}
+          />
+          <FormInput
+            name="photo"
+            label="Photo URL"
+            control={control}
+            errors={errors}
+          />
+          <FormInput
+            name="address"
+            label="Address"
+            control={control}
+            errors={errors}
+          />
+          <FormInput
+            name="city"
+            label="City"
+            control={control}
+            errors={errors}
+            sm={6}
+          />
+          <FormInput
+            name="state"
+            label="State"
+            control={control}
+            errors={errors}
+            sm={3}
+          />
+          <FormInput
+            name="zip"
+            label="ZIP"
+            control={control}
+            errors={errors}
+            sm={3}
+          />
+          <FormInput
+            name="phone"
+            label="Phone"
+            control={control}
+            errors={errors}
+          />
+          <FormInput
+            name="email"
+            label="Email"
+            control={control}
+            errors={errors}
+          />
+          <FormInput
+            name="notes"
+            label="Notes"
+            control={control}
+            errors={errors}
+            type="textarea"
+            minRows={4}
+            maxRows={12}
+          />
+          <Grid item xs={12} sm={4}>
+            <Button
+              variant="text"
+              fullWidth
+              sx={{ mt: 2 }}
+              onClick={() => navigate("/")}
+            >
+              Cancel
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={4}>
             <Button
               type="button"
               variant="contained"
@@ -72,7 +158,7 @@ const ProfileForm = () => {
               Clear Form
             </Button>
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={4}>
             <Button
               type="submit"
               variant="contained"
@@ -80,7 +166,7 @@ const ProfileForm = () => {
               fullWidth
               sx={{ mt: 2 }}
             >
-              {profile?.id ? 'Update Profile' : 'Create Profile'}
+              {profile?.id ? "Update Profile" : "Create Profile"}
             </Button>
           </Grid>
         </Grid>

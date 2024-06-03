a40dbd89ff1c06006f750512304f4b2d53fe1904
Jose Rubio <jadrubio@gmail.com>
2024-06-03 11:41:51 -0400

    Removed unused imports

--- src/features/profile/__tests__/ProfileList.test.tsx
+++ src/features/profile/__tests__/ProfileList.test.tsx
@@ -4,10 +4,8 @@ import "@testing-library/jest-dom/extend-expect";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { ProfileList } from "../ProfileList";
-import { profileList } from "../../../store/profileSlice";
 import { Profile } from "../profileUtils";
-import profileReducer from "../../../store/profileSlice"; // Adjust this import to the actual path
-import * as reduxHooks from "react-redux";
+import profileReducer from "../../../store/profileSlice";
 
 jest.mock("../ProfileLineItem", () => ({
   ProfileLineItem: ({ profile }: { profile: Profile }) => (
